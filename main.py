1478ba4ed586c502dbd63b9fca81bdc3d2f91340
mihirsar <mihir.sardesai03@gmail.com>
2024-05-29 23:28:55 +0530

    init

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ user_params = {
     "notMinor": "yes",
 }
 
+headers = {
+    "X-USER-TOKEN": TOKEN
+    }
+
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
@@ -23,49 +27,46 @@ user_params = {
 ## Creation of graph
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
  
-headers = {
-    "X-USER-TOKEN": TOKEN
-}
+def create_graph():
 
-graph_config = {
+    graph_config = {
     "id": GRAPH_ID,
     "name": "SuryaNamaskar",
     "unit": "reps",
     "type": "int",
     "color": "momiji", # momiji is red
-}
+    }
 
-# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-# print(response.text)
+    response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+    # print(response.text)
 
-today = datetime.now()
 
+today = datetime.now() # todays date details
 ## posting a pixel
 pixel_post_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
-# def post_pixel(reps):
-#     """posts pixel"""
-#     pixel_config = {
-#         "date": today.strftime("%Y%m%d"),
-#         "quantity": "21"
-#     }
-#     response = requests.post(url=pixel_post_endpoint, json=pixel_config, headers=headers)
-#     # print(response.text)
+def post_pixel(reps):
+    """posts pixel"""
+    pixel_config = {
+        "date": today.strftime("%Y%m%d"),
+        "quantity": f"{reps}"
+    }
+    response = requests.post(url=pixel_post_endpoint, json=pixel_config, headers=headers)
+    # print(response.text)
 
 ## modify pixel
-# def update_pixel(dateToModify):
-#     """updates pixel takes input as yyyymmdd format"""
-#     update_pixel_endpoint = f"{pixel_post_endpoint}/{dateToModify}"
-#     # pixel_config = {
-#     #     "date": today.strftime("%Y%m%d"),
-#     #     "quantity": "21"
-#     # }
-#     response = requests.put(url=update_pixel_endpoint, headers=headers)
-
-dateToModify = "20240529"
-update_pixel_endpoint = f"{pixel_post_endpoint}/{dateToModify}"
-updated_pixel_config = {
-        "date": dateToModify,
-        "quantity": "17"
+def update_pixel(dateToModify, reps):
+    """updates pixel takes input as yyyymmdd format"""
+    update_pixel_endpoint = f"{pixel_post_endpoint}/{dateToModify}"
+    updated_pixel_config = {
+        "date": f"{dateToModify}",
+        "quantity": f"{reps}"
     }
-response = requests.put(url=update_pixel_endpoint, json=updated_pixel_config, headers=headers)
-print(response.text)
+    response = requests.put(url=update_pixel_endpoint, json=updated_pixel_config, headers=headers)
+    # print(response.text)
+
+## delete pixel 
+# /v1/users/<username>/graphs/<graphID>/<yyyyMMdd>
+def delete_pixel(dateToDelete):
+    del_pixel_endpoint = f"{pixel_post_endpoint}/{dateToDelete}"
+    response = requests.delete(url=del_pixel_endpoint, headers=headers)
+
